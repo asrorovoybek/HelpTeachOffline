747c46e409f359603be3b2f1fbd5636312808218
asrorovoybek <akountbuzish@gmail.com>
2026-04-21 23:06:44 +0500

    Add files via upload

--- app/src/main/java/com/helpteach/offline/ui/ScheduleScreen.kt
+++ app/src/main/java/com/helpteach/offline/ui/ScheduleScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.helpteach.offline.data.Lesson
 import com.helpteach.offline.viewmodel.AppViewModel
