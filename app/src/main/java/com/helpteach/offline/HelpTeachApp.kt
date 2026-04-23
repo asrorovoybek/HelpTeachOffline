dae65abe5b0bbfee3514dc1bfa4602f264745e64
asrorovoybek <akountbuzish@gmail.com>
2026-04-23 22:08:42 +0500

    Delete app directory

--- app/src/main/java/com/helpteach/offline/HelpTeachApp.kt
+++
@@ -1,8 +0,0 @@
-package com.helpteach.offline
-
-import android.app.Application
-import com.helpteach.offline.data.AppDatabase
-
-class HelpTeachApp : Application() {
-    val database by lazy { AppDatabase.getDatabase(this) }
-}
